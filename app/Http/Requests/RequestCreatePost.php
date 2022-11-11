460d08aeff70686ddbb9415fd9968f90f274dc6d
Hasan <hsmusayev@gmail.com>
2022-11-11 15:46:49 +0400

    add cache, model observers, eloquent trips, tricks

--- app/Http/Requests/RequestCreatePost.php
+++ app/Http/Requests/RequestCreatePost.php
@@ -22,12 +22,10 @@ class RequestCreatePost extends FormRequest
      * @return array
      */
     public function rules()
-    {
-        $id = $this->request->get('user_id');
+        {
         return [
             'title' => ['required'],
             'body' =>  ['required'],
-            'user_id' => [auth()->user()->id]
         ];
     }
 }
